78a65cb7bb3dac23f18dd4fd4141026268563673
FarhanAdka <farhanadkareynaldi21@gmail.com>
2023-11-03 02:28:06 +0700

    First Commit

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    function index()
+    {
+        return view('login');
+    }
+
+    function login(Request $request){
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ],[
+            'email.required'=>'Email wajib diisi',
+            'password.required'=>'Password wajib diisi'
+        ]
+    );
+
+    $infologin = [
+        'email'=>$request->email,
+        'password'=>$request->password,
+    ];
+
+    if(Auth::attempt($infologin)){
+        if(Auth::user()->role == 'mahasiswa'){
+            return redirect('user/mahasiswa');
+        }
+        elseif(Auth::user()->role == 'operator'){
+            return redirect('user/operator');
+        }
+        elseif(Auth::user()->role == 'dosen wali'){
+            return redirect('user/dosenWali');
+        }
+        elseif(Auth::user()->role == 'departemen'){
+            return redirect('user/departemen');
+        }
+    }
+    else{
+        return redirect('')->withErrors('Email atau pasword tidak sesuai')->withInput();
+    }
+}
+    function logout(){
+        Auth::logout();
+        return redirect('');
+    }
+}
