116519918a8e8887da007547620716c4f59b702c
MTF51 <mustafa.iswardana@gmail.com>
2023-11-04 08:54:28 +0700

    Penyesuaian

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Supprt\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    
+    function index(){
+        return view('admin');
+    }
+
     function mahasiswa(){
-        echo "selamat datang mahasiswa";
-        echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='/logout'>Logout >></a>";
+        return view('admin');;
     }
     function operator(){
-        echo "selamat datang operator";
-        echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='/logout'>Logout >></a>";
+        return view('admin');
     }
     function dosenWali(){
-        echo "selamat datang dosen wali";
-        echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='/logout'>Logout >></a>";
+        return view('admin');
     }
     function departemen(){
-        echo "selamat datang departemen";
-        echo "<h1>". Auth::user()->name ."</h1>";
-        echo "<a href='/logout'>Logout >></a>";
+        return view('admin');
     }
 }
